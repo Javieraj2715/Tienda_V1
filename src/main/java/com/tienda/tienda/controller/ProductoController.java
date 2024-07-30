045f5deacd14daea33732a5bba55b7c0bdd9606a
Javieraj2715 <pepon.012728@gmail.com>
2024-07-29 20:53:06 -0600

    Semana 11
    Asociaciones y Queries

--- src/main/java/com/tienda/tienda/controller/ProductoController.java
+++ src/main/java/com/tienda/tienda/controller/ProductoController.java
@@ -1,8 +1,11 @@
 package com.tienda.Tienda.controller;
- 
-import com.tienda.tienda.Service.CategoriaService;
+
 import com.tienda.tienda.domain.Producto;
+
 import lombok.extern.slf4j.Slf4j;
+
+
+import com.tienda.tienda.Service.CategoriaService;
 import com.tienda.tienda.Service.ProductoService;
 import com.tienda.tienda.Service.service.FirebaseStorageServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,33 +16,37 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
- 
- 
+
+
 @Controller
 @Slf4j
-@RequestMapping("/producto") //(/producto/listado)
+@RequestMapping("/producto") //(/producto)
 public class ProductoController {
- 
+
     @Autowired
     private ProductoService productoService;
+    
     @Autowired
     private CategoriaService categoriaService;
-    
- 
+
     @GetMapping("/listado")
     public String inicio(Model model) {
         var productos = productoService.getProductos(false);
+        var categorias = categoriaService.getCategorias(false);
         model.addAttribute("productos", productos); //lista de productos
+        model.addAttribute("categorias", categorias);
         model.addAttribute("totalProductos", productos.size()); //
         return "/producto/listado";
     }
+    
      @GetMapping("/nuevo")
     public String productoNuevo(Producto producto) {
         return "/producto/modifica";
     }
- 
+
     @Autowired
     private FirebaseStorageServiceImpl firebaseStorageService;
+    
     @PostMapping("/guardar")
     public String productoGuardar(Producto producto,
             @RequestParam("imagenFile") MultipartFile imagenFile) {        
@@ -54,20 +61,21 @@ public class ProductoController {
         productoService.save(producto);
         return "redirect:/producto/listado";
     }
- 
+
     @GetMapping("/eliminar/{idProducto}")
     public String productoEliminar(Producto producto) {
         productoService.delete(producto);
         return "redirect:/producto/listado";
     }
- 
+
     @GetMapping("/modificar/{idProducto}")
     public String productoModificar(Producto producto, Model model) {
         producto = productoService.getProducto(producto);
-        var categoria = categoriaService.getCategorias(false);
-        model.addAttribute("categorias", categoria);
+        var categorias = categoriaService.getCategorias(false);
+        model.addAttribute("categorias", categorias);
         model.addAttribute("producto", producto);
         return "/producto/modifica";
     }   
-
+    
+    
 }
