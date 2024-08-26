4acfe3215fcbcc66f056f21076c79654983ac87f
Javieraj2715 <pepon.012728@gmail.com>
2024-08-26 14:22:56 -0600

    Ultimos Cambios

--- src/main/java/com/tienda/tienda/Service/UsuarioDetailsService.java
+++ src/main/java/com/tienda/tienda/Service/UsuarioDetailsService.java
@@ -1,7 +1,6 @@
 package com.tienda.tienda.Service;
 
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 
